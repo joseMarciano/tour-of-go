10ef95d984bd0dda0f50e8136238d089448add78
joseMarciano <marcianojosepaulo@gmail.com>
2023-05-03 21:37:41 -0300

    feat: :sparkles: working with types

---
+++ type/I/type-assertions.go
@@ -0,0 +1,41 @@
+package main
+
+/*
+package main
+
+import "fmt"
+
+func main() {
+	var i interface{} = "hello"
+
+	s := i.(string)
+	fmt.Println(s)
+
+	s, ok := i.(string)
+	fmt.Println(s, ok)
+
+	f, ok := i.(float64)
+	fmt.Println(f, ok)
+
+	f = i.(float64) // panic
+	fmt.Println(f)
+}
+
+*/
+import "fmt"
+
+func main() {
+	var i interface{} = "hello"
+
+	s := i
+	fmt.Println(s)
+
+	s, ok := i.(string)
+	fmt.Println(s, ok)
+
+	f, ok := i.(float64)
+	fmt.Println(f, ok)
+
+	f = i.(float64) // panic
+	fmt.Println(f)
+}
